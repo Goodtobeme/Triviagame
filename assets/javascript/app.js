cac41a598d30e14206a37e15269496ca0e97b715
Goodtobeme <eugenebeckwith@ymail.com>
2017-12-24 12:53:56 -0600

    Adding first part of Triviagame, need to add timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,83 @@
+ 
+console.log("Hello World");
+
+ var questions = [{
+   "question": "What city won the 1986 Super Bowl?",
+   "option1" "Miami",
+   "option1" "New England",
+   "option1" "Pittsburgh",
+   "option1" "Chicago",
+   "answer": "Chicago"
+ },
+ {
+   "question": "What year was the the last carbureted vehicle sold ",
+   "option1" "1985",
+   "option1" "1990",
+   "option1" "1994",
+   "option1" "1999",
+   "answer": "1994"
+ },
+ {
+   "question": "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "answer": ""
+ },
+ {
+   "question": "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "answer": ""
+ },
+ {
+   "question": "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "answer": ""
+ },
+ {
+   "question": "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "answer": ""
+ },
+ {
+   "question": "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "answer": ""
+ },
+ {
+   "question": "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "answer": ""
+ },
+ {
+   "question": "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "answer": ""
+ },
+ {
+   "question": "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "option1" "",
+   "answer": ""
+ }];
