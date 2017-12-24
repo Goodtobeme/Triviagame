09f7f960d2bec6a044c9be991a97b17622d82bb8
Goodtobeme <eugenebeckwith@ymail.com>
2017-12-24 14:02:32 -0600

    Changes to all files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,83 +1,41 @@
- 
-console.log("Hello World");
+function check() {
 
- var questions = [{
-   "question": "What city won the 1986 Super Bowl?",
-   "option1" "Miami",
-   "option1" "New England",
-   "option1" "Pittsburgh",
-   "option1" "Chicago",
-   "answer": "Chicago"
- },
- {
-   "question": "What year was the the last carbureted vehicle sold ",
-   "option1" "1985",
-   "option1" "1990",
-   "option1" "1994",
-   "option1" "1999",
-   "answer": "1994"
- },
- {
-   "question": "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "answer": ""
- },
- {
-   "question": "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "answer": ""
- },
- {
-   "question": "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "answer": ""
- },
- {
-   "question": "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "answer": ""
- },
- {
-   "question": "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "answer": ""
- },
- {
-   "question": "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "answer": ""
- },
- {
-   "question": "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "answer": ""
- },
- {
-   "question": "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "option1" "",
-   "answer": ""
- }];
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var correct = 0;
+
+    if(question1 == "Chicago") {
+      correct++;
+    }
+    if(question2 == "Springfield") {
+      correct++;
+    }
+    if(question3 == "1905") {
+      correct++;
+    }
+
+    var message = ["Excellent Job", "You are doing Ok", "Need to study your History More!"];
+    var pictures = ["assets/images/win.gif", "assets/images/ha.gif", "assets/images/loser.gif"];
+
+    var outlook;
+
+    if (correct < 1) {
+      outlook = 2;
+    }
+
+    if (correct > 0 && correct <3) {
+      outlook = 1;
+    }
+
+    if (correct > 2) {
+      outlook = 0;
+    }
+
+    document.getElementById("after_submit").style.visibility = "visible";
+
+    document.getElementById("message").innerHTML = message[outlook];
+    document.getElementById("number_correct").innerHTML = " You got " + correct + " correct. ";
+    document.getElementById("picture").src =pictures[outlook];
+
+}
